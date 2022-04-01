de762ea3d256dfbcb53e5092bd98c03d3a99701b
pranita1295 <pranita@aliteprojects.in>
2022-04-01 16:48:08 +0530

    remove nodeModule

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    
    // Send the response body as "Hello World"
    response.end('Welcome to Aliteprojects... \n');
-}).listen(8081);
+}).listen(3000);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+console.log('Server running at http://127.0.0.1:3000/');
